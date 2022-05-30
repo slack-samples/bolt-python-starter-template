4469b8add2eba2c1a5dd80451ccd997ce2a069e3
Kazuhiro Sera <seratch@gmail.com>
2022-05-30 17:46:32 +0900

    Use 125 chars as max line length

--- listeners/views/sample_view.py
+++ listeners/views/sample_view.py
@@ -10,12 +10,8 @@ def sample_view_callback(view, ack: Ack, body: dict, client: WebClient, logger: 
         sample_user_value = body["user"]["id"]
         provided_values = view["state"]["values"]
         logger.info(f"Provided values {provided_values}")
-        sample_input_value = provided_values["input_block_id"]["sample_input_id"][
-            "value"
-        ]
-        sample_convo_value = provided_values["select_channel_block_id"][
-            "sample_dropdown_id"
-        ]["selected_conversation"]
+        sample_input_value = provided_values["input_block_id"]["sample_input_id"]["value"]
+        sample_convo_value = provided_values["select_channel_block_id"]["sample_dropdown_id"]["selected_conversation"]
 
         client.chat_postMessage(
             channel=sample_convo_value,
