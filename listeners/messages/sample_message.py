dc84f3563b815a0f08820ed7200ed6bb64775bdf
William Bergamin <wbergamin@slack-corp.com>
2022-08-05 13:00:47 -0400

    clean up code

--- listeners/messages/sample_message.py
+++ listeners/messages/sample_message.py
@@ -4,7 +4,7 @@ from slack_bolt import BoltContext, Say
 from slack_sdk import WebClient
 
 
-def sample_message_callback(context: BoltContext, client: WebClient, say: Say, logger: Logger):
+def sample_message_callback(context: BoltContext, say: Say, logger: Logger):
     try:
         greeting = context["matches"][0]
         say(f"{greeting}, how are you?")
